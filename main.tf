6bfd1d1fe05d88f9d9c084251972215ad345505f
Pradeeptiwari1234 <56465468+Pradeeptiwari1234@users.noreply.github.com>
2023-01-19 16:43:04 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,39 @@
+
+terraform {
+  cloud {
+    organization = "pradeeporg1"
+    workspaces {
+      name = "test" 
+      }
+        }
+  required_providers {
+    conjur = {
+      source  = "registry.terraform.io/cyberark/conjur"
+      version = "0.6.3"
+    }
+  }
+}
+
+provider "conjur" {
+  appliance_url = "https://conjur-server"
+  account       = "myaccount"
+  login         = "admin"
+  api_key       = "ashgdysb"
+  ssl_cert      = "auhdibicsacns"
+  # ssl_cert_path = "/etc/conjur.pem"
+}
+
+data "conjur_secret" "dbpass" {
+  name = "terraform-example/dbpass"
+}
+
+output "dbpass-to-output" {
+  value     = data.conjur_secret.dbpass.value
+  sensitive = true
+}
+
+resource "local_file" "dbpass-to-file" {
+  content         = data.conjur_secret.dbpass.value
+  filename        = "${path.module}/../dbpass"
+  file_permission = "0664"
+}
